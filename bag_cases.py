7a99ad2e284a03220c95035084e255bebd0af821
Timothee Mollet <timothee.mollet@gmail.com>
2020-07-13 23:31:07 +0200

    plot age

--- bag_cases.py
+++ bag_cases.py
@@ -4,7 +4,12 @@ import pandas as pd
 import requests
 import io
 import argparse
+import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.pylab as pl
+import matplotlib.dates as mdates
+import cycler
 all_link='https://www.bag.admin.ch/dam/bag/de/dokumente/mt/k-und-i/aktuelle-ausbrueche-pandemien/2019-nCoV/covid-19-basisdaten-fallzahlen.xlsx.download.xlsx/Dashboards_1&2_COVID19_swiss_data_pv.xlsx'
 
 
@@ -45,6 +50,49 @@ print(df.groupby(['age']).sum())
 print(df.xs('BE', level='canton', drop_level=False))
 print(df.xs('BE', level='canton', drop_level=False).groupby(['age']).sum())
 print(df.sum())
-df.xs('BE', level='canton', drop_level=False).groupby(['date']).sum().plot()
-df.xs('BE', level='canton', drop_level=False).groupby(['date']).sum().rolling(window=7).mean().plot()
+#df.xs('BE', level='canton', drop_level=False).groupby(['date']).sum().plot()
+#df.xs('BE', level='canton', drop_level=False).groupby(['date']).sum().rolling(window=7).mean().plot()
+#plt.show()
+date_age=df.groupby(['date', 'age']).sum()[['cases']]
+date=df.groupby(['date']).sum()[['cases']]
+
+date_age =date_age.groupby(level='age').cases.apply(lambda x: x.rolling(window=7).mean()).unstack()[:-1]
+date=date.rolling(window=7).mean().dropna()[:-1]
+
+date_age_relative=date_age.divide(date_age.sum(axis=1), axis=0).dropna()
+
+
+fig, ax1 = plt.subplots(1,1)
+fig.set_size_inches(18.5,10.5, forward=True)
+#date.plot(ax=ax1, kind='line', linewidth=4, color='black', logy=True)
+ax1.plot(pd.to_datetime(date.index), date.to_numpy(), color='black', linewidth=4, label='Neue Fälle')
+
+ax2 = ax1.twinx()
+ax1.set_yscale('log')
+cmap = matplotlib.cm.get_cmap('PuOr')
+color =  cmap(np.linspace(0,1,10))
+
+ax2.stackplot(pd.to_datetime(date_age_relative.index), date_age_relative.to_numpy().T,labels=date_age_relative.columns, colors=color) 
+#date_age_relative.plot(ax=ax2, kind='area', stacked=True ,colormap='PuOr')
+ax1.set_zorder(10)
+ax1.yaxis.grid(color='black', linestyle='dashed')
+
+mloc = mdates.MonthLocator()
+wloc = mdates.WeekdayLocator()
+
+ax1.xaxis.set_major_locator(wloc)
+ax1.xaxis.set_major_formatter(mdates.DateFormatter('%b %d'))
+#ax1.xaxis.set_minor_locator(wloc)
+ax1.xaxis.grid(color='black', linestyle='dashed')
+ax1.patch.set_visible(False)
+ax2.set_ylim([0,1])
+
+handles1, labels1 = ax1.get_legend_handles_labels()
+handles2, labels2 = ax2.get_legend_handles_labels()
+
+ax1.legend(handles1, labels1, loc='lower center', bbox_to_anchor=(0.5,1.0))
+ax2.legend(handles2[::-1], labels2[::-1], loc='center left', bbox_to_anchor=(1.025,0.5))
+plt.title('Altersverteilung und neue Fälle in der Schweiz', y=1.05)
+plt.margins(x=0)
+plt.subplots_adjust(left=0.05, right=0.9, top=0.9, bottom=0.05)
 plt.show()
